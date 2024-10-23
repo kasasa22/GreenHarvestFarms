e3b966dfca54c3e14b7f2ba451e9e9abe3b93643
Kasasa Livingstone Trevor <kasasa.livingstonetrevor@students.mak.ac.ug>
2024-10-23 13:15:12 +0300

    Initial setup of GreenHarvest Farms Web App

---
+++ frontend/src/App.js
@@ -0,0 +1,12 @@
+// frontend/src/App.js
+import React from 'react';
+
+function App() {
+   return (
+      <div className="App">
+         <h1>Welcome to GreenHarvest Farms</h1>
+      </div>
+   );
+}
+
+export default App;
